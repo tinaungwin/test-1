e8ac64499a4abc5d106c5e393b471a5bc5a81b11
Steve TAW <stevetaw@gmail.com>
2012-10-07 21:33:30 +0800

    A test to see conflict

--- hello.c
+++ hello.c
@@ -3,7 +3,11 @@
 int main ( int argc, char** argv)
 {
 // A test program to try out Git.
+<<<<<<< HEAD
 // This line is added at the serve end.
+=======
+// This line is added at the client end.
+>>>>>>> A test to see conflict
 
 	printf("Hello World\n");
 }
