47d8776cba002d656da61244552d69745bc55554
Steve TAW <stevetaw@gmail.com>
2012-10-07 23:19:19 +0800

    This is a commit message from commandline.

---
+++ test/test/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  test
+//
+//  Created by Tin Aung Win on 7/10/12.
+//  Copyright (c) 2012 Tin Aung Win. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
